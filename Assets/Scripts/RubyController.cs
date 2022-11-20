e301606e678758b3b9550c05be5c8403d91008b9
yuankeqing <1240376341@qq.com>
2022-11-20 21:43:00 +0800

    删除无用的导入

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
-using UnityEditor;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
